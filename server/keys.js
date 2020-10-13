40acaf0873aae6f266f7490f3dd164dce4dbd855
toka <tokaelgindy98@gmail.com>
2020-10-14 01:07:54 +0200

    create login route and send token

--- server/keys.js
+++ server/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-	MONGOURI: "mongodb+srv://toka:1234abcd@cluster0.6lios.mongodb.net/<dbname>?retryWrites=true&w=majority"
+	MONGOURI: "mongodb+srv://toka:1234abcd@cluster0.6lios.mongodb.net/<dbname>?retryWrites=true&w=majority",
+	JWT_SECRET:"effegwewrgrgw4"
 }
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 const mongoose = require('mongoose')
 const User = mongoose.model("User")
-
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const {JWT_SECRET} = require('../keys')
 
 router.get('/', (req,res) => {
 res.send("hello")
@@ -20,24 +22,54 @@ router.post('/signup', (req,res) =>{
 		if(savedUser) {
 			return res.status(422).json({error: "user already exists with that email"})	
 		}
-		const user = new User({
-			email,
-			password,
-			name
+		bcrypt.hash(password, 12)
+		.then(hashedpassword => {
+			const user = new User({
+				email,
+				password: hashedpassword,
+				name
+			})
+
+			user.save()
+			.then(user => {
+				res.json({message:"saved successfully"})
+			})
+			.catch(err => {
+				console.log(err)
+			})			
 		})
 
-		user.save()
-		.then(user => {
-			res.json({message:"saved successfully"})
-		})
-		.catch(err => {
-			console.log(err)
-		})
 	})
 	.catch(err => {
 		console.log(err)
 	})
 })
 
+router.post('/signin', (req, res) => {
+	const { email, password } = req.body
+	if(!email || !password){
+		res.status(422).json({error: "please provide email or password"})
+	}
+	User.findOne({email:email})
+	.then(savedUser => {
+		if(!savedUser){
+			return res.status(422).json({error: "Invalid Email or password"})
+		}
+		bcrypt.compare(password, savedUser.password)
+		.then(doMatch => {
+			if(doMatch){
+				//res.json({message: "successfully signed in"})			
+				const token = jwt.sign({_id: savedUser._id}, JWT_SECRET)
+				res.json({token})
+			}
+			else{
+			return res.status(422).json({error: "Invalid Email or password"})				
+			}
+		})
+		.catch(err => {
+			console.log(err)
+		})
+	})
+})
 
 module.exports = router;
