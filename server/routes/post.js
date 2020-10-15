0dd5e18a0439fd9e5a0d817ef61ed90c351ca793
toka <tokaelgindy98@gmail.com>
2020-10-15 21:03:48 +0200

    all posts route / posts of a signed in user route

--- server/routes/post.js
+++ server/routes/post.js
@@ -4,6 +4,16 @@ const mongoose = require('mongoose')
 const requireLogin = require('../middleware/requireLogin')
 const Post = mongoose.model("Post")
 
+router.get('/allpost', (req, res) => {
+	Post.find()
+	.populate("postedBy", "_id name")//get _id and name only
+	.then(posts => {
+		res.json({posts})
+	})
+	.catch(err => {
+		console.log(err)
+	})
+})
 
 router.post('/createpost', requireLogin, (req,res) =>{
 	const { title, body } = req.body
@@ -20,6 +30,17 @@ router.post('/createpost', requireLogin, (req,res) =>{
 	post.save()
 	.then(result => {
 		res.json({post:result})
+	})
+	.catch(err => {
+		console.log(err)
+	})
+})
+
+router.get('/myposts', requireLogin, (req, res) => {
+	Post.find({postedBy:req.user._id})
+	.populate("postedBy", "_id name")
+	.then(mypost => {
+		res.json({mypost})
 	})
 	.catch(err => {
 		console.log(err)
