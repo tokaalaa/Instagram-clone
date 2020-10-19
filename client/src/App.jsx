c710ee1bc3113ef9cdd97468f44033d49c22a2c4
toka <tokaelgindy98@gmail.com>
2020-10-19 19:06:06 +0200

    create profile page/ rename Login file to Signin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import './App.css';
 import{ BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/screens/Home';
 import Profile from './components/screens/Profile';
-import Signin from './components/screens/Login';
+import Signin from './components/screens/SignIn';
 import Signup from './components/screens/Signup';
 
 
