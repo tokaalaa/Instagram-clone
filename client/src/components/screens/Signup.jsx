50da6952d116572a4ce2583f91670674b038a63d
toka <tokaelgindy98@gmail.com>
2020-10-19 23:58:14 +0200

    working on user signup by posting data

--- client/src/components/screens/Signup.jsx
+++ client/src/components/screens/Signup.jsx
@@ -1,7 +1,39 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-
+import React, {useState} from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import M from 'materialize-css'
 const Signup = ()=>{
+	const history = useHistory()
+	const [name,setName] =useState("")
+	const [email,setEmail] =useState("")
+	const [password,setPassword] =useState("")
+	const PostData =()=>{
+		if(!/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(email)){
+			M.toast({html: "Invalid Email", classes:"#c62828 red darken-3"})
+			return
+		}
+		fetch("/signup",{
+			method:"post",
+			headers:{
+				"Content-Type":"application/json"
+			},
+			body:JSON.stringify({
+				name,
+				password,
+				email
+			})
+		}).then(res=>res.json())
+		.then(data=>{
+			if(data.error){
+				M.toast({html: data.error, classes:"#c62828 red darken-3"})
+			}
+			else{
+				M.toast({html: data.message, classes:"#43a047 green darken-1"})
+				history.push('/signin')
+			}
+		})
+	}
+
+
 	return (
 		<div className="mycard">
 			<div className="card auth-card input-field">
@@ -9,16 +41,24 @@ const Signup = ()=>{
 				<input
 				type="text"
 				placeholder="name"
+				value={name}
+				onChange={(e)=>setName(e.target.value)}
 				/>
 				<input
 				type="text"
 				placeholder="email"
+				value={email}
+				onChange={(e)=>setEmail(e.target.value)}
 				/>
 				<input
 				type="text"
 				placeholder="password"
+				value={password}
+				onChange={(e)=>setPassword(e.target.value)}
 				/>
-				<button className="btn waves-effect waves-light #64b5f6 blue darken-1">
+				<button className="btn waves-effect waves-light #64b5f6 blue darken-1"
+				onClick={()=>PostData()}
+				>
 				 SignUp  
 				</button>
 				<h5>
